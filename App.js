ceeb40d387b953f44b27ce99a158298311663007
Anita RASOA <anita.ras@onja.org>
2020-11-06 07:31:01 +0300

    Creating the speed type game in react

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
-/**
- * Challenge: build the basic structure of our game
- *
- * 1. <h1> title at the top
- * 2. <textarea> for the box to type in
- *      (tip: React normalizes <textarea /> to be more like <input />,
- *      so it can be used as a self-closing element and uses the `value` property
- *      to set its contents)
- * 3. <h4> ti display the amount of time remaining
- * 4. <button> to start the game
- * 5. Another <h1> to display the word count
- */
+import React, { useEffect, useState, useRef } from 'react';
+
+function App() {
+    const START_TIME = 10;
+
+    const [text, setText] = useState('');
+    const [timeRemaining, setTimeRemaining] = useState(START_TIME);
+    const [isStart, setIsStart] = useState(false);
+    const [finalResult, setFinalResult] = useState(0);
+    const textareaRef = useRef(null);
+
+    function startGame() {
+        setIsStart(true);
+        setTimeRemaining(START_TIME);
+        setText("");
+        textareaRef.current.disabled = false;
+        textareaRef.current.focus();
+    }
+
+    function endGame() {
+        setIsStart(false);
+        const numWords = calculateWordCount(text);
+        setFinalResult(numWords);
+    }
+
+    const handleChange = (e) => {
+        const { value } = e.target
+        setText(value);
+    }
+
+    useEffect(() => {
+        if (isStart && timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1);
+            }, 1000);
+        } else if (timeRemaining === 0) {
+            endGame();
+        }
+    }, [isStart, timeRemaining]);
+
+    const calculateWordCount = (text) => {
+        const wordCounts = text.trim().split(' ');
+        const wordLength = wordCounts.filter(word => word !== '');
+        return wordLength.length;
+    }
+
+    return (
+        <>
+            <h1>Speed typing Game</h1>
+            <textarea ref={textareaRef} disabled={!isStart} onChange={handleChange} value={text} />
+            <h4>Remaining amount of time: {timeRemaining}</h4>
+            <button disabled={isStart} onClick={startGame}>Start</button>
+            <h1>The word count: {finalResult}</h1>
+        </>
+    )
+}
+
+export default App;
